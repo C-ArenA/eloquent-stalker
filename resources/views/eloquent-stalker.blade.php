25acffe33d6208f0393a82b637cf2317735a365f
C-ArenA <carlostata.ca@gmail.com>
2024-11-04 16:29:49 -0400

    call css

--- resources/views/eloquent-stalker.blade.php
+++ resources/views/eloquent-stalker.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Eloquent Stalker</title>
-    @vite('resources/css/app.css')
+    {{ Vite::useHotFile('vendor/eloquent-stalker/eloquent-stalker.hot')
+        ->useBuildDirectory("dist")
+        ->withEntryPoints(['resources/css/app.css', 'resources/js/app.js']) }}
 </head>
 
 <body>
